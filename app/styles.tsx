a49b72d10fcbc3d91227e96cbb38a3f8e2a5a675
khian calderon <ckhianvictory@gmail.com>
2025-06-15 16:13:32 +0800

    Separated Styles

---
+++ app/styles.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  mainContent: {
+    padding: 25
+  },
+  templateTitle: {
+    fontSize: 18,
+    fontWeight: "bold"
+  },
+  href: {
+    color: "blue",
+    textDecorationLine: "underline"
+  }
+})
